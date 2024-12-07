6d35641185a762ff8598eb0d4249130776fae2e9
MHD <mahdimazhabi@gmail.com>
2024-12-07 10:45:12 +0330

    fix bog to components Herosections

--- src/pages/Home/components/HeroSection/HeroSection.tsx
+++ src/pages/Home/components/HeroSection/HeroSection.tsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <section className="py-12">
-      <div className="container mx-auto flex flex-col md:flex-row  items-center justify-between px-4">
+    <section className="py-12 ">
+      <div className="container flex flex-col items-center justify-between px-4 mx-auto md:flex-row">
         {/* بخش تصویر */}
-        <div className="w-full md:w-1/2 flex justify-center order-1 md:order-2 mb-8 md:mb-0">
+        <div className="flex justify-end order-1 w-full mb-8 md:w-1/2 md:order-2 md:mb-0">
           <img
             src={ImgDeveloper}
             alt="Developer"
@@ -17,27 +17,27 @@ const HeroSection = () => {
         </div>
 
         {/* بخش متن */}
-        <div className="w-full md:w-1/2 text-center md:text-right  order-2 md:order-1">
+        <div className="order-2 w-full text-center md:w-1/2 md:text-right md:order-1">
           <h1 className="text-3xl  md:text-5xl font-black text-gray-800 dark:text-white leading-[50px] md:leading-[100px]">
             با ما یاد بگیرید و درآمد کسب کنید؛ جایی که آموزش و کار در کنار
             هم‌اند!
           </h1>
 
-          <p className="mt-4 text-lg text-gray-600 font-bold dark:text-gray-300">
+          <p className="mt-4 text-lg font-bold text-gray-600 dark:text-gray-300">
             یادگیری مهارت‌های جدید هیچ‌وقت این‌قدر ساده و کاربردی نبوده است.
           </p>
-          <div className="lg:flex lg:justify-stretch  items-center gap-4 mt-10">
+          <div className="items-center gap-4 mt-10 lg:flex lg:justify-stretch">
             <div>
-              <Button className="bg-blue-500 p-6" rounded={"full"}>
+              <Button className="p-6 bg-blue-500" rounded={"full"}>
                 از این مسیر ها شروع کن{" "}
               </Button>
             </div>
-            <div className="flex items-center justify-center gap-4 mt-10 lg:mt-0 md:mt-0 font-medium">
+            <div className="flex items-center justify-center gap-4 mt-10 font-medium lg:mt-0 md:mt-0">
               <span>دوره های رایگان</span>
               <Link to={""}>
                 <Button
                   icon={Play}
-                  className="bg-pink-800 border-none p-6"
+                  className="p-6 bg-pink-800 border-none"
                   rounded={"full"}
                   size={"icon"}
                 />
