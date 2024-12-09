b600b8186b1fc2ee538ccc223d4fa2add2ee4228
MHD <mahdimazhabi@gmail.com>
2024-12-09 23:08:22 +0330

    edite componets MenuItems

--- src/shared/layouts/MenuItems/MenuItems.tsx
+++ src/shared/layouts/MenuItems/MenuItems.tsx
@@ -3,24 +3,34 @@ import { Link } from "react-router-dom";
 const MenuItems = () => {
   return (
     <ul className="flex-col gap-2 py-4 lg:p-0 lg:flex lg:flex-row">
-      <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
-        <Link to="">خانه</Link>
-      </li>
-      <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
-        <Link to="">اساتید</Link>
-      </li>
-      <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
-        <Link to="/Projects">پروژه ها</Link>
-      </li>
-      <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
-        <Link to="/Courses">دوره ها</Link>
-      </li>
+      <Link to="">
+        <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
+          خانه
+        </li>
+      </Link>
+      <Link to="">
+        <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
+          اساتید
+        </li>
+      </Link>
+      <Link to="/Projects">
+        <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
+          پروژه ها
+        </li>
+      </Link>
+      <Link to="/Courses">
+        <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
+          دوره ها
+        </li>
+      </Link>
       <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
         <Link to="/Blog">وبلاگ</Link>
       </li>
-      <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
-        <Link to="">درباره ما</Link>
-      </li>
+      <Link to="">
+        <li className="w-full p-2 rounded dark:hover:bg-white/10 hover:bg-blue-700 hover:text-white lg:w-auto">
+          درباره ما
+        </li>
+      </Link>
     </ul>
   );
 };
