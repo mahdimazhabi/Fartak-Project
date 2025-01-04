a3f78b9be80b3f73c7dd71864a6bd9fdae0019d4
MHD <mahdimazhabi@gmail.com>
2025-01-04 23:45:24 +0330

    authlayout

--- src/shared/layouts/AuthLayout/AuthLayout.tsx
+++ src/shared/layouts/AuthLayout/AuthLayout.tsx
@@ -1,23 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Img from "@/assets/Img/Rectangle 57.png";
 
-
 const AuthLayout = () => {
   return (
     <main>
-      <div className="flex min-h-screen py-2 overflow-hidden lg:px-9">
-        {/* بخش تصویر */}
+      <div className="flex items-center  min-h-screen overflow-hidden  lg:px-3">
         <div className="hidden lg:flex">
           <img
             src={Img}
             alt="Login Illustration"
-            className="w-[40vw] h-[97.5vh] object-cover  rounded-lg "
+            className="w-[40vw] h-[97.5vh] object-cover"
           />
         </div>
-
-        {/* بخش فرم ورود */}
         <Outlet />
-      
       </div>
     </main>
   );
