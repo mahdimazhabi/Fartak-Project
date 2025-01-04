7ddb7af1bcd9afc02d26a95c98732417f200f97e
MHD <mahdimazhabi@gmail.com>
2025-01-05 00:35:12 +0330

    edit to schema registration

--- src/shared/schema/schemaRegistration.tsx
+++ src/shared/schema/schemaRegistration.tsx
@@ -2,12 +2,13 @@ import * as yup from "yup";
 
 export const schemaRegistration = yup.object().shape({
   lastNamefristname: yup.string().required("نام و نام خانوداگی الزامی است"),
-  mobile: yup
-    .string()
-
-    .required("شماره تلفن الزامی است"),
+  mobile: yup.string().required("شماره تلفن الزامی است"),
   password: yup
     .string()
     .min(6, "رمز عبور باید حداقل 6 کاراکتر باشد")
     .required("رمز عبور الزامی است"),
+  role: yup
+    .string()
+    .oneOf(["فریلنسر", "دانشجو"], "نقش انتخابی نامعتبر است")
+    .required("انتخاب نقش الزامی است"),
 });
