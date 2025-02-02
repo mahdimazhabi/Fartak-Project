fb4a1c6cd5ef9a1441e95f743af4c4223d028f9f
MHD <mahdimazhabi@gmail.com>
2025-02-02 13:56:40 +0330

    edit to bg color to components service

--- src/pages/Home/components/Services/Services.tsx
+++ src/pages/Home/components/Services/Services.tsx
@@ -13,7 +13,7 @@ const Services = () => {
         />
       </div>
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
-        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 rounded-lg lg:p-8 p-4 border-2 border-yellow-400 ">
+        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 rounded-lg lg:p-8 p-4 border-2 border-yellow-400 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-600">
           <h1 className="text-xl font-bold text-yellow-400 mb-3 dark:text-shadow-xl">
             فریلنسینگ
           </h1>
@@ -21,7 +21,7 @@ const Services = () => {
             فرصت‌های متعددی برای ارائه خدمات مستقل در زمینه‌های مانند توسعه وب
           </p>
         </div>
-        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 rounded-lg border-2 border-teal-400 lg:p-8 p-4">
+        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 rounded-lg border-2 border-teal-400 lg:p-8 p-4 bg-gradient-to-r from-teal-300 via-teal-500 to-teal-600">
           <h1 className="text-xl font-bold text-teal-400 mb-3 dark:text-shadow-xl">
             دوره‌های آموزشی
           </h1>
@@ -30,13 +30,13 @@ const Services = () => {
             زبان‌ها، هنرها و...
           </p>
         </div>
-        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 border-2 border-pink-400 rounded-lg lg:p-8 p-4">
+        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 border-2 border-pink-400 rounded-lg lg:p-8 p-4 bg-gradient-to-r from-pink-300 via-pink-500 to-pink-600">
           <h1 className="text-xl font-bold text-pink-400 mb-3 dark:text-shadow-xl">
             پاسخگویی سریع
           </h1>
           <p>پاسخگویی سریع به سوالات و راهنمایی‌های فنی در طول شبانه‌روز</p>
         </div>
-        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 border-2 border-blue-400 rounded-lg lg:p-8 p-4">
+        <div className="dark:bg-[#242a38] bg-white bg-opacity-10 border-2 border-blue-400 rounded-lg lg:p-8 p-4 bg-gradient-to-r from-blue-300 via-blue-500 to-blue-600">
           <h1 className="text-xl font-bold text-blue-400 mb-3 dark:text-shadow-xl">
             دوره‌های آموزشی (تکراری)
           </h1>
