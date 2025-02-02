cc919c8e1286d0b01e7700eaca2c5305ca6e9525
MHD <mahdimazhabi@gmail.com>
2025-02-02 21:12:46 +0330

    fix bog

--- src/shared/layouts/TeachingLayout/TeachingLayout.tsx
+++ src/shared/layouts/TeachingLayout/TeachingLayout.tsx
@@ -2,8 +2,7 @@ import CoursesTeaching from "@/pages/Teaching/components/CoursesTeaching";
 import TopProfessors from "@/pages/Teaching/components/TopProfessors";
 import TeachingFartak from "@/pages/Teaching/components/TeachingFartak";
 import TeacherEnglish from "@/pages/Teaching/components/TeacherEnglish";
-import { CoursesHeadareTeaching } from "@/pages/Teaching/components/CoursesHeadareTeaching";
-// import CoursesHeadareTeaching from "@/pages/Teaching/components/CoursesHeadareTeaching";
+import CoursesHeadareTeaching from "@/pages/Teaching/components/CoursesHeadareTeaching";
 const TeachingLayout = () => {
   return (
     <>
