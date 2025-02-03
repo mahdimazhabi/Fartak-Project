b3aadbb41c33cf9cdef12ac56f51ca1f69b3b00f
MHD <mahdimazhabi@gmail.com>
2025-02-03 11:36:52 +0330

    edit to style hovercomponents...

--- src/shared/components/HoverComponetsTeacher.tsx
+++ src/shared/components/HoverComponetsTeacher.tsx
@@ -18,51 +18,51 @@ const HoverComponetsTeacher = () => {
     <section className="p-11 w-full  absolute  top-[7rem]">
       <div className="bg-[#FFFFFF] dark:bg-slate-800 p-6   rounded-lg z-50">
         <ul className="flex flex-col items-start space-y-4 w-full border-l-4 max-w-fit pl-6 ">
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <BocKIcon />
             <span>ابتدایی</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3  ">
             <SchoolIcon />
             <span>متوسطه اول</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base   dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3">
             <BackpackIcon />
             <span>متوسطه دوم</span>
           </li>
-          <li className="relative left-2 flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="relative left-2 flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <GraduationcapIcon />
             <span>کنکور سراری</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <ForeignlanguageIcon />
             <span>زبان های خارجی</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <BocK2Icon />
             <span>دروس دانشگاهی</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <SoftwareIcon />
             <span>نرم افزار ها</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <ProgammerIcon />
             <span>برنامه نویسی</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <SongIcon />
             <span>موسیقی</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <ThetestIcon />
             <span>آزمون های خارجی</span>
           </li>
-          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <SkillIcon />
             <span>هنر و مهارت ها</span>
           </li>
-          <li className="relative left-2 flex items-center cursor-pointer text-black dark:text-white font-medium text-base  ">
+          <li className="relative left-2 flex items-center cursor-pointer text-black dark:text-white font-medium text-base  dark:hover:bg-white/20 rounded hover:bg-black/20 transition-colors  px-3 ">
             <ExerciseIcon />
             <span>ورزش</span>
           </li>
