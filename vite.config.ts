f91af057bcaa463a74046873bb62ea6fa4db4b73
MHD <mahdimazhabi@gmail.com>
2025-02-10 12:58:59 +0330

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"), // مسیر درست برای alias
     },
   },
+  base: "./", // مسیرهای نسبی برای دیپلوی در Plesk
 });
