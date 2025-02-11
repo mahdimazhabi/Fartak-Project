f4693042b25819a47ed00a696283b46ce9984bc6
MHD <mahdimazhabi@gmail.com>
2025-02-11 15:03:58 +0330

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ایمپورت BrowserRouter
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -11,10 +10,8 @@ const queryClient = new QueryClient({});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/fartak">
-        <App />
-        <ReactQueryDevtools />
-      </BrowserRouter>
+      <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>
 );
