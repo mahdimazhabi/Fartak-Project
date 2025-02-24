8f0d57d6c5633ed969a948531ad2bfacbb7de0e1
MHD <mahdimazhabi@gmail.com>
2025-02-24 12:22:28 +0330

    fix big to rendring img

--- src/pages/Teaching/components/TopProfessors.tsx
+++ src/pages/Teaching/components/TopProfessors.tsx
@@ -66,7 +66,7 @@ const TopProfessors = () => {
               >
                 <img
                   className="w-32 h-32 object-cover rounded-full"
-                  src={`https://www.backend.fartakproject.ir/upload/teacherusers/${prof.imageName}`}
+                  src={`https://www.backend.fartakproject.ir/upload/teacheruserImages/${prof.imageName}`}
                   // alt={`تصویر پروفایل ${prof.name}`}
                 />
 
